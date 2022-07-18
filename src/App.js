ab4c94ee1aaf3de47cdb96e2c5ae6d3b99358fab
Mehdi01-git <louihranim@gmail.com>
2022-07-18 22:24:52 +0200

    Core Files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { birds } from "./data";
+
+function App() {
+  return (
+    <div className="App">
+      {birds.map((bird, index) => {
+        return (
+          <span className="container">
+            <h3 className="title">{bird}</h3>
+            <img
+              src={`https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird}`}
+              className="Bird"
+              styles={`color: white`}
+              key={index}
+            />
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
