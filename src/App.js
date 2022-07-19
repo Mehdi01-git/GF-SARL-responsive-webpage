2d750a34cc5e3f1a13baf3200ed3d02939616bd6
Mehdi01-git <louihranim@gmail.com>
2022-07-19 14:38:39 +0200

    Infinite Scroll Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { birds } from "./data";
 
 function App() {
+  const [scrollIndex, setScrollIndex] = useState(5);
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      if (
+        window.innerHeight + e.target.documentElement.scrollTop >=
+        e.target.documentElement.scrollHeight
+      ) {
+        setScrollIndex(scrollIndex + 5);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, [scrollIndex]);
+
   return (
     <div className="App">
-      {birds.map((bird, index) => {
-        return (
-          <span className="container">
-            <h3 className="title">{bird}</h3>
-            <img
-              alt="birdImg"
-              src={`https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird}`}
-              className="Bird"
-              styles={`color: white`}
-              key={index}
-            />
-          </span>
-        );
-      })}
+      {birds
+        .filter((bird, index) => index < scrollIndex)
+        .map((bird, index) => {
+          return (
+            <span key={index} className="container">
+              <h3 className="title">{bird}</h3>
+              <img
+                alt="birdImg"
+                src={`https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird}`}
+                className="Bird"
+                styles={`color: white`}
+              />
+            </span>
+          );
+        })}
     </div>
   );
 }
